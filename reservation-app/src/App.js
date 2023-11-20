e0ea1cfa963da6c8538f2e1d732dba6f9af611e6
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-20 12:28:26 +0900

    메뉴 추가

--- reservation-app/src/App.js
+++ reservation-app/src/App.js
@@ -7,6 +7,29 @@ import "react-calendar/dist/Calendar.css";
 
 import "./App.css";
 
+const menuList = {
+  appetizer: [
+    { name: "양송이수프", price: 6000 },
+    { name: "타파스", price: 5500 },
+    { name: "시저샐러드", price: 8000 },
+  ],
+  main: [
+    { name: "티본스테이크", price: 55000 },
+    { name: "바비큐립", price: 54000 },
+    { name: "해산물파스타", price: 35000 },
+    { name: "크리스마스파스타", price: 25000 },
+  ],
+  dessert: [
+    { name: "초코케이크", price: 15000 },
+    { name: "아이스크림", price: 5000 },
+  ],
+  drink: [
+    { name: "제로콜라", price: 3000 },
+    { name: "레드와인", price: 60000 },
+    { name: "샴페인", price: 25000 },
+  ],
+};
+
 const MenuPage = ({
   handleMenuClick,
   handleNextButtonClick,
@@ -14,16 +37,58 @@ const MenuPage = ({
   order,
 }) => (
   <div className="menu-container">
-    <h2>주문 메뉴</h2>
+    <h2>애피타이저</h2>
     <div className="menu-list">
-      <div className="menu-item" onClick={() => handleMenuClick("음식1")}>
-        음식1
-      </div>
-      <div className="menu-item" onClick={() => handleMenuClick("음식2")}>
-        음식2
-      </div>
-      {/* 추가적인 메뉴 항목들을 여기에 추가 */}
+      {menuList.appetizer.map((item) => (
+        <div
+          key={item.name}
+          className="menu-item"
+          onClick={() => handleMenuClick(item.name)}
+        >
+          {`${item.name} (${item.price.toLocaleString()}원)`}
+        </div>
+      ))}
     </div>
+
+    <h2>메인</h2>
+    <div className="menu-list">
+      {menuList.main.map((item) => (
+        <div
+          key={item.name}
+          className="menu-item"
+          onClick={() => handleMenuClick(item.name)}
+        >
+          {`${item.name} (${item.price.toLocaleString()}원)`}
+        </div>
+      ))}
+    </div>
+
+    <h2>디저트</h2>
+    <div className="menu-list">
+      {menuList.dessert.map((item) => (
+        <div
+          key={item.name}
+          className="menu-item"
+          onClick={() => handleMenuClick(item.name)}
+        >
+          {`${item.name} (${item.price.toLocaleString()}원)`}
+        </div>
+      ))}
+    </div>
+
+    <h2>음료</h2>
+    <div className="menu-list">
+      {menuList.drink.map((item) => (
+        <div
+          key={item.name}
+          className="menu-item"
+          onClick={() => handleMenuClick(item.name)}
+        >
+          {`${item.name} (${item.price.toLocaleString()}원)`}
+        </div>
+      ))}
+    </div>
+
     {showOrderSummary && (
       <div className="order-summary">
         <h2>주문 요약</h2>
@@ -34,6 +99,7 @@ const MenuPage = ({
         </ul>
       </div>
     )}
+
     <Link to="/order-summary">
       <button onClick={handleNextButtonClick}>다음으로</button>
     </Link>
